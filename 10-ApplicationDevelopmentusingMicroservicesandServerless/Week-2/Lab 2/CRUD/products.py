38c1e351b4042c30bce2d7073a2424d3aaf83173
ShahandFahad <shahandfahadmad@gmail.com>
2023-11-15 20:49:05 +0500

    Course 10 Week 2 Lab 1 & 2

---
+++ 10-ApplicationDevelopmentusingMicroservicesandServerless/Week-2/Lab 2/CRUD/products.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+import json
+from flask_cors import CORS
+
+app = Flask("Product Server")
+CORS(app)
+
+
+products = [
+    {'id': 143, 'name': 'Notebook', 'price': 5.49},
+    {'id': 144, 'name': 'Black Marker', 'price': 1.99}
+]
+
+#
+#Add all the REST API end-points here
+#
+# Example request - http://localhost:5000/products
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+# Example request - http://localhost:5000/products/144 - with method GET
+@app.route('/products/<id>', methods=['GET'])
+def get_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    return jsonify(product)
+
+# Example request - http://localhost:5000/products - with method POST
+@app.route('/products', methods=['POST'])
+def add_product():
+    products.append(request.get_json())
+    return '', 201
+
+# Example request - http://localhost:5000/products/144 - with method PUT
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+    id = int(id)
+    updated_product = json.loads(request.data)
+    product = [x for x in products if x["id"] == id][0]
+    for key, value in updated_product.items():
+        product[key] = value
+    return '', 204
+
+# Example request - http://localhost:5000/products/144 - with method DELETE
+@app.route('/products/<id>', methods=['DELETE'])
+def remove_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    products.remove(product)
+    return '', 204
+
+
+
+app.run(port=5000,debug=True)

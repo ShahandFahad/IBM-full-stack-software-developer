62e5da09ff10779405774e60bbd9b532eb851562
ShahandFahad <shahandfahadmad@gmail.com>
2023-11-26 23:37:35 +0500

    Course 11 Week 1 Lab 01

---
+++ 11-FullStackCloudDevelopmentCapstoneProject/Week-1/Lab01OverviewApplication-StaticPages/server/djangoapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+# from .models import related models
+
+
+# Register your models here.
+
+# CarModelInline class
+
+# CarModelAdmin class
+
+# CarMakeAdmin class with CarModelInline
+
+# Register models here
